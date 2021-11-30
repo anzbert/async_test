fb6cb01f3ca317c006ea2e09227d65d69faece48
anzbert <anzbert@gmail.com>
2021-11-30 15:56:44 +0800

    seperate to fn

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,28 @@
 // API: GET from /fact (parameter max_length for string length limit)
 // receive json object with fact and length property
 
+use async_std::fs::ReadDir;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Deserialize, Serialize)]
 struct CatFact {
     fact: String,
     length: u64,
 }
 
+fn blocking_fact(source: &str) -> Result<CatFact, surf::Error> {
+    async_std::task::block_on(surf::get(source).recv_json::<CatFact>())
+}
+
 fn main() {
     let cf = "https://catfact.ninja/fact";
-    let fact = match async_std::task::block_on(surf::get(cf).recv_json::<CatFact>()) {
+    let fact = match blocking_fact(cf) {
         Ok(x) => x.fact,
-        Err(e) => format!("Sorry, there was an error getting a cat fact: {}", e),
+        Err(e) => format!(
+            "Sorry, there was an error getting a cat fact: \n\n       {}",
+            e
+        ),
     };
 
-    println!("Cat Fact: {}", fact);
+    println!("\n{}\n", fact);
 }
